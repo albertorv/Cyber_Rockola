f8df728c409cd1cf4e46d8b52579f746e806ccac
albertorv <albertorv.07@gmail.com>
2015-06-24 13:10:11 -0600

    configuración de la bd

--- cyberApp/database/migrations/2014_10_12_000000_create_users_table.php
+++ cyberApp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
+            $table->is_bool('is_admin');
             $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
